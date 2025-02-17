fef4f22f310500bf71a163f8d3427b46312067fc
Arunas Jokubynas <20095400@mail.wit.ie>
2025-02-17 20:59:03 +0000

    Backend core setup, tool installation

---
+++ backend/app.js
@@ -0,0 +1,19 @@
+// Importing the Express library and creating an instance of the Express app
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware to parse the request body as JSON, log HTTP requests, and enable CORS
+app.use(express.json());
+app.use(require('morgan')('dev'));
+app.use(require('cors')());
+
+//Defining route for the root URL
+app.get('/', (req, res) => {
+    res.send('Dish Discover Backend is running');
+});
+
+// Start the server and listen on the port
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
