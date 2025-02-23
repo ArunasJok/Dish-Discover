118578f6b8d5d2d17d1df5cfaafc1f3a105ec356
Arunas Jokubynas <20095400@mail.wit.ie>
2025-02-23 20:04:08 +0000

    Introduced basic error handling on routes

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,11 @@
-// Importing the Express library and creating an instance of the Express app
+// Description: Main entry point for the backend server. This file is responsible for setting up the server, connecting to the database, and defining the routes for the API.
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const connectDB = require('./config/db'); // Import database connection
-
+const errorHandler = require('./middlewares/errorHandler'); // Import error handler middleware
+// Import routes
 const authenticationRoutes = require('./routes/authenticationRoutes');
 const recipeRoutes = require('./routes/recipeRoutes');
 const externalRoutes = require('./routes/externalRoutes');
@@ -16,10 +17,12 @@ const port = process.env.PORT || 3000;
 // Connect to MongoDB
 connectDB();
 
-// Middleware to parse the request body as JSON, log HTTP requests, and enable CORS
+// Middleware to parse the request body as JSON, log HTTP requests, and enable CORS. 
+// Error handler middleware is used to catch any errors that occur during the request processing.
 app.use(express.json());
 app.use(require('morgan')('dev'));
 app.use(require('cors')());
+
 
 //Route integration
 app.use('/api/auth', authenticationRoutes);
@@ -31,6 +34,15 @@ app.use('/api/ai', aiRoutes);
 app.get('/', (req, res) => {
     res.send('Dish Discover Backend is running');
 });
+
+// Catch-all route errors
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use(errorHandler);
 
 // Start the server and listen on the port
 app.listen(port, () => {
