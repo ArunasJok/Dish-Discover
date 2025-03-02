666342532cdab20de16b4bdd297e81269d3f9dc4
Arunas Jokubynas <20095400@mail.wit.ie>
2025-03-02 17:07:09 +0000

    Setting up frontend - installing React

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Dish Discover</h1>
+      <p>This is the Dish Discover frontend.</p>
+    </div>
+  );
+}
+
+export default App;
