22e4a8c5828524080e5d2e8d8ae138ead8b36b19
Arunas Jokubynas <20095400@mail.wit.ie>
2025-03-06 15:13:46 +0000

    Setting up backend for migration to Azure

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,19 +1,16 @@
 // Database connection configuration
-// The connectDB function is responsible for connecting to the MongoDB database.
-const mongoose = require('mongoose');
 
-const dbURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/dishdiscover';
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(dbURI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+    // Using the connection string from Azure Cosmos DB
+    const conn = await mongoose.connect(process.env.MONGO_CONNECTION_STRING, {     
     });
-    console.log('MongoDB connected successfully');
+    console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error('Error connecting to MongoDB:', error);
-    process.exit(1); // Exit process with failure
+    console.error(`Error connecting to MongoDB: ${error}`);
+    process.exit(1);
   }
 };
 
